dc85e5054db94035fc630d8ca6b98942223a99d2
Dwight Spencer <dspencer@computekindustries.com>
2020-06-18 15:43:27 -0500

    (feat) support command flags
    Changes
      - Moved default command flags to variable
    
    Added
      - parameter testing
      - parameter overriding via environment variable
      - parameter expansion via docker COMMAND values

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 
-exec qemu-system-i386 -fda floppy.img -boot a -curses
+## Establish default command flags or override with Environment variables
+OPTS=${OPTS:-"-fda floppy.img -boot a -curses"}
+
+## If Command has value expand defaults
+[[ $# -gt 0 ]] && OPTS += "$*"
+
+## Exectue qemu with command flags
+exec qemu-system-i386 ${OPTS}
